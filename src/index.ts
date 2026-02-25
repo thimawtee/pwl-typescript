2fc7ab8468009ec34ffb9d6b01da0bf9f9523c6a
thimawtee <timothyketapang@gmail.com>
2026-02-25 04:46:35 +0000

    update nama

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama: string, umur: number): string {
     return 'Selamat datang '+nama+' di PWL, umur '+mahasiswa+' adalah ' +umur;
 }
-const mahasiswa = "Andi";
+const mahasiswa = "Timothy";
 const umur: number = 19;
 console.log(hello(mahasiswa, umur));
