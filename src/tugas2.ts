9ccb42c3994c8c08b1faabc2fc1659a64c8d7f2e
thimawtee <timothyketapang@gmail.com>
2026-02-25 17:23:27 +0000

    Mengubah isi kode tugas 2 pada modul 4 - Menggunakan Interface

--- src/tugas2.ts
+++ src/tugas2.ts
@@ -5,14 +5,18 @@ interface Buku {
     tersedia: boolean;
 }
 
-const buku1: Buku = {
+function cetakBuku(buku: Buku): void {
+    console.log("Judul: " + buku.judul);
+    console.log("Pengarang: " + buku.pengarang);
+    console.log("Tahun Terbit: " + buku.tahunTerbit);
+    console.log("Tersedia: " + buku.tersedia);
+}
+
+const data: Buku = {
     judul: "Pemrograman Web Lanjut",
     pengarang: "Timothy Walukow",
     tahunTerbit: 2024,
     tersedia: true
 };
 
-console.log("Judul:", buku1.judul);
-console.log("Pengarang:", buku1.pengarang);
-console.log("Tahun Terbit:", buku1.tahunTerbit);
-console.log("Tersedia:", buku1.tersedia);
+cetakBuku(data);
